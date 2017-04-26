e47ab3d72a8f9caf8605e3b2a65bfea31fa7565d
Cale Tilford <combinekidd@gmail.com>
2017-04-26 12:48:47 +0100

    Update UI elements

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -1,4 +1,4 @@
 class Experiment < ApplicationRecord
-  has_many :experiment_tasks
-  has_many :experiment_results
+  has_many :experiment_tasks, dependent: :destroy
+  has_many :experiment_results, dependent: :destroy
 end
