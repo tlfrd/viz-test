57b43ba7fc84650d5c82957a1166a0d781b085b7
Cale Tilford <combinekidd@gmail.com>
2017-05-03 23:46:46 +0100

    Add basic results page

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,5 +1,7 @@
 class ResultsController < ApplicationController
   http_basic_authenticate_with name: ENV["APP_USER"], password: ENV["APP_SECRET"]
   def index
+    @experiment_results = ExperimentResult.all.order(id: :desc)
+    @experiment_task = ExperimentTask.all.order(id: :asc)
   end
 end
