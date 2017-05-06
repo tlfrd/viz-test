0ba540852878288ebcc87375fd3b6214398df12e
Cale Tilford <combinekidd@gmail.com>
2017-05-06 22:36:16 +0100

    Calculate averages for experiment task results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,7 +2,7 @@ class ResultsController < ApplicationController
   http_basic_authenticate_with name: ENV["APP_USER"], password: ENV["APP_SECRET"]
   def index
     @experiment_results = ExperimentResult.all.order(id: :desc)
-    @experiment_task = ExperimentTask.all.order(id: :asc)
+    @experiment_tasks = ExperimentTask.all.order(id: :asc)
   end
 
   def experiment_task_result
@@ -11,5 +11,8 @@ class ResultsController < ApplicationController
     if (@visualisation.html.start_with?('http'))
       @visualisation.html = open(@visualisation.html).read
     end
+    @experiment_task_results = @experiment_task.experiment_task_results
+    @average_time = ExperimentTaskResult.get_average_time(@experiment_task_results)
+    @average_coordinates = ExperimentTaskResult.get_average_coordinates(@experiment_task_results)
   end
 end
--- app/models/experiment_task_result.rb
+++ app/models/experiment_task_result.rb
@@ -1,4 +1,30 @@
 class ExperimentTaskResult < ApplicationRecord
   belongs_to :experiment_result
   belongs_to :experiment_task
+
+  def self.get_average_time(experiment_tasks_results)
+    total = 0
+    count = 0
+    experiment_tasks_results.each do |experiment_task_result|
+      experiment_task_result.result.each do |json_result|
+        total += json_result[1]["time"]
+        count += 1
+      end
+    end
+    return total/count
+  end
+
+  def self.get_average_coordinates(experiment_task_results)
+    total_x = 0
+    total_y = 0
+    count = 0
+    experiment_task_results.each do |experiment_task_result|
+      experiment_task_result.result.each do |json_result|
+        total_x += json_result[1]["coordinates"][0]
+        total_y += json_result[1]["coordinates"][1]
+        count += 1
+      end
+    end
+    return [total_x / count, total_y / count]
+  end
 end
