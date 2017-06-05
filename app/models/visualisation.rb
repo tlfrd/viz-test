33f290ddc9ec20815118a6bf5c9de53b135de56a
Cale Tilford <combinekidd@gmail.com>
2017-06-06 00:12:33 +0100

    Remove images from visualisation index

--- app/models/visualisation.rb
+++ app/models/visualisation.rb
@@ -1,3 +1,8 @@
 class Visualisation < ApplicationRecord
   has_many :tasks, dependent: :destroy
+
+  # write something to show all visualisations connected
+  def self.get_experiments(visualisation_id)
+    viz = Visualisation.find_by_id(visualisation_id)
+  end
 end
