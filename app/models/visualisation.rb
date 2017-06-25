e6db9801f6c4ddd2bec4a582ac063086ea58cab9
Cale Tilford <combinekidd@gmail.com>
2017-06-25 16:46:20 +0100

    Dummy commit

--- app/models/visualisation.rb
+++ app/models/visualisation.rb
@@ -2,7 +2,6 @@ class Visualisation < ApplicationRecord
   has_many :tasks, dependent: :destroy
   acts_as_taggable_on :colour, :type
 
-  # write something to show all visualisations connected
   def self.get_experiments(visualisation_id)
     viz = Visualisation.find_by_id(visualisation_id)
   end
